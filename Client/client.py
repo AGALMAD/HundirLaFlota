cf3cdcccc5a29763dc8ea40bf348f27eb6433cf4
AGALMAD <agalmad@g.educaand.es>
2025-02-05 18:07:56 +0100

    el cliente no inicializa los barcos

--- Client/client.py
+++ Client/client.py
@@ -1,52 +1,9 @@
 import json
 import socket, threading
 
-import board, ship
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket initialization
 client.connect(( '192.168.1.83' , 7976 )) #connecting client to server
-
-ships = []
-
-
-def initBoards () :
-        #Pide barco 1
-        print("Enter Ship 1 :")
-        positions = []
-        for i in range(5):
-            position = input("Enter position #" + str(i + 1) + " : " )
-            positions.append(position)
-        new_ship = ship.Ship(positions)
-        ships.append(new_ship)
-
-        #Pide barco 2
-        print("Enter Ship 2 :")
-        positions = []
-        for i in range(4):
-            position = input("Enter position #" + str(i + 1) + " : " )
-            positions.append(position)
-        new_ship = ship.Ship(positions)
-        ships.append(new_ship)
-
-        #Pide barcos 3 y 4
-        for i in range(2):
-            positions = []
-            print("Enter Ship #" + str(i + 3) + " : " )
-            for j in range(3):
-                position = input("Enter position #" + str(j + 1) + " : ")
-                positions.append(position)
-            new_ship = ship.Ship(positions)
-            ships.append(new_ship)
-
-        #Pide barcos 5
-        print("Enter Ship 5 :")
-        positions = []
-        for i in range(2):
-            position = input("Enter position #" + str(i + 1) + " : " )
-            positions.append(position)
-
-        new_ship = ship.Ship(positions)
-        ships.append(new_ship)
 
 
 def receive () :
